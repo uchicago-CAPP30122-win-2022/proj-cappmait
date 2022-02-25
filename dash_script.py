b8015615b85e364aa7c4d67364924289d88eb996
Maggie Guo Chen <90563834+maggiechen615@users.noreply.github.com>
2022-02-24 20:54:50 -0600

    Delete dash_script.py

--- dash_script.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px  # (version 4.7.0 or higher)
-import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
-
-app = Dash(__name__)
-
-# import and clean the data
-df = pd.read_csv("owid_covid_data.csv")
