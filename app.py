f0d8edfde587e15dc48cd0663bc54682459fdd89
Guo Chen <gchen22@linux4.cs.uchicago.edu>
2022-02-24 22:43:00 -0600

    added map codes, need to check the err msg

--- app.py
+++ app.py
@@ -11,8 +11,15 @@ product = product[["Indicator", "ReporterISO3A", "Reporter", "ProductCode", "Pro
 product.loc[product["Indicator"]=="Merchandise imports by product group - annual", "Indicator"] = "Import"
 product.loc[product["Indicator"]=="Merchandise exports by product group - annual", "Indicator"] = "Export"
 product["Year"] = product["Year"].astype('category')
+covid_data = pd.read_csv("WHO-COVID-19-global-data.csv")
 
-# Define Layout
+# Clean the data (specifically for covid_data)
+covid_data[['Year', 'Month', 'Day']] = covid_data['Date_reported'].\
+                                    str.split("-", expand = True)
+covid_data_2020 = covid_data[covid_data.Year == '2020'].groupby(\
+                                'Country')['Cumulative_cases'].sum()
+
+# Define Layout (dash components should be inside app layout)
 app.layout = html.Div([
 
     # Title
@@ -43,8 +50,11 @@ app.layout = html.Div([
 
 ])
 
+# callback is supposed to connect graphs with dash components
+# added worldmap as an output, added component to be shown on the dashboard
 @app.callback(
-    [Output(component_id="barplot", component_property="figure"),
+    [Output(component_id="worldmap", component_property="figure"),
+    Output(component_id="barplot", component_property="figure"),
     Output(component_id="sankeyplot", component_property="figure"),
     Output(component_id="lineplot", component_property="figure")],
     [Input(component_id="slt_country", component_property="value")]
@@ -53,8 +63,19 @@ app.layout = html.Div([
 def update_output(val_selected):
     
     df = product[product["Reporter"]==val_selected]
+    df_l = covid_data_2020[covid_data_2020['Country'] == val_selected]
+    container = 'The dashboard is for the selected country: {}'.format(val_selected)
 
-    
+    # try with the tentative covid data and use plotly express
+    worldmap = px.choropleth(
+        data_frame=df_l,
+        locations='Country',
+        color='Cumulative_cases',
+        hover_data=['Country', 'Cumulative_cases'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        template='plotly_dark'
+    )
+
     bar_plt = px.bar(df[df["ProductCode"] == "TO"], x="Indicator", y="Value", 
                  color="Year", barmode="group")
 
@@ -66,7 +87,7 @@ def update_output(val_selected):
     line_plt = px.bar(df, x="Year", y="Value", 
                  color="Indicator", barmode="group")
 
-    return (bar_plt, sankey_plt, line_plt)
+    return (container, worldmap, bar_plt, sankey_plt, line_plt)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
