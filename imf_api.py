af1d1edfbb6e41f243cfe58a9c3c7a209590951c
taishi <taishi1991911@gmail.com>
2022-02-24 18:43:16 -0600

    created a function to extract export data

--- imf_api.py
+++ imf_api.py
@@ -1,41 +1,50 @@
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
 
 url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/'
-key = 'CompactData/IFS/Q.JP+GB.TXG_D_FOB_USD_IX?startPeriod=2017&endPeriod=2020'
 
-'''
-Key should be
-    CompactData/{database ID}/{frequency}
-    .{item1 from dimension1}+{item2 from dimension1}+{item N from dimension1}
-    .{item1 from dimension2}+{item2 from dimension2}+{item M from dimension2}
-    ?startPeriod={start date}&endPeriod={end date}
+key = 'DataStructure/DOT'
+dimension_list = requests.get(f'{url}{key}').json()\
+            ['Structure']['KeyFamilies']['KeyFamily']\
+            ['Components']['Dimension']
 
-This retrieves
-    data type: CompactData
-    database ID: the International Financial Statistics (IFS) series.
-    frequency: quarterly (frequency: Q)
-    dimension1: countries, which are Japan and the U.K. (reference area: JP, GB)
-    dimension2: External Trade, Goods, Deflator/Unit Value of Exports, 
-                Free on Board (FOB), in US Dollars, Index (indicator: TXG_D_FOB_USD_IX)
-    time period: startPeriod = 2017 and endPeriod = 2020
-'''
+key = f'CodeList/{dimension_list[1]["@codelist"]}'
+code_list_d2 = requests.get(f'{url}{key}').json()\
+	    ['Structure']['CodeLists']['CodeList']['Code']
+country_codes = {}
+for code in code_list_d2:
+    country_codes[code['@value']] = code['Description']['#text']
 
-data = (requests.get(f'{url}{key}').json()['CompactData']['DataSet']['Series'])
+def extract_export_data(target_country):
+    '''
+    Extract one country's trading data with its trading partners in 2019 and 2020.
+    Inputs:
+        target_country(str): a country's code
+    Outputs:
+        trading data(pd.DataFrame): trading data of the target country
+        missing_country(list):
+            country codes which the target country doesn't have data about
+            * including the case when there is data in either 2019 or 2020.
+    '''
+    code_list = list(country_codes.keys())
 
-# In the following process, I only use Japan as my test. (Japan is in data[0])
-country_code = data[0]['@REF_AREA']
+    # Separating the key because there seems to be URL's length limitation.
+    key_d3 = []
+    for i in range(4):
+        key_d3.append('+'.join(code_list[i*70 : min((i+1)*70, len(country_codes))]))
 
-data_list = [[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
-             for obs in data[0]['Obs']]
-df = pd.DataFrame(data_list, columns=['date', 'value'])
-df = df.set_index(pd.to_datetime(df['date']))['value'].astype('float')
-
-plt.figure()
-title = f'{country_code}, External Trade, Goods, Deflator/Unit Value of Exports,\n\
-        Free on Board (FOB)'
-df.plot(title=title, colormap='Set1')
-plt.xlabel('date')
-plt.ylabel('US Dollars')
-plt.show()
+    trading_data = {}
+    missing_country = []
+    for k in key_d3:
+        key = f'CompactData/DOT/A.{target_country}.TXG_FOB_USD.{k}\
+            ?startPeriod=2019&endPeriod=2020'
+        data = requests.get(f'{url}{key}').json()['CompactData']['DataSet']['Series']
+        for s in data:
+            df_dict_col = {}
+            if 'Obs' not in s.keys() or type(s['Obs']) != list:
+                missing_country.append(s['@COUNTERPART_AREA'])
+                continue
+            for i in s['Obs']:
+                df_dict_col[i['@TIME_PERIOD']] = round(float(i['@OBS_VALUE']), 1)
+            trading_data[s['@COUNTERPART_AREA']] = df_dict_col
+    return pd.DataFrame(trading_data).T, missing_country
