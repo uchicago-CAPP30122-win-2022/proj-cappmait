fecc59f3b917a3afd300a872920a0ce264578223
taishi <taishi1991911@gmail.com>
2022-02-25 11:57:37 -0600

    added import function

--- imf_api.py
+++ imf_api.py
@@ -14,6 +14,12 @@ code_list_d2 = requests.get(f'{url}{key}').json()\
 country_codes = {}
 for code in code_list_d2:
     country_codes[code['@value']] = code['Description']['#text']
+code_list = list(country_codes.keys())
+
+# Separating the key because there seems to be URL's length limitation.
+key_d3 = []
+for i in range(4):
+    key_d3.append('+'.join(code_list[i*70 : min((i+1)*70, len(country_codes))]))
 
 def extract_export_data(target_country):
     '''
@@ -26,15 +32,7 @@ def extract_export_data(target_country):
             country codes which the target country doesn't have data about
             * including the case when there is data in either 2019 or 2020.
     '''
-    code_list = list(country_codes.keys())
-
-    # Separating the key because there seems to be URL's length limitation.
-    key_d3 = []
-    for i in range(4):
-        key_d3.append('+'.join(code_list[i*70 : min((i+1)*70, len(country_codes))]))
-
     trading_data = {}
-    missing_country = []
     for k in key_d3:
         key = f'CompactData/DOT/A.{target_country}.TXG_FOB_USD.{k}\
             ?startPeriod=2019&endPeriod=2020'
@@ -42,14 +40,33 @@ def extract_export_data(target_country):
         for s in data:
             df_dict_col = {}
             if 'Obs' not in s.keys() or type(s['Obs']) != list:
-                missing_country.append(s['@COUNTERPART_AREA'])
                 continue
             for i in s['Obs']:
                 df_dict_col[i['@TIME_PERIOD']] = round(float(i['@OBS_VALUE']), 1)
             trading_data[s['@COUNTERPART_AREA']] = df_dict_col
-    return pd.DataFrame(trading_data).T, missing_country
+    return pd.DataFrame(trading_data).T
 
 
 def extract_import_data(target_country):
-    pass
-
+    '''
+    Extract one country's trading data with its trading partners in 2019 and 2020.
+    Inputs:
+        target_country(str): a country's code
+    Outputs:
+        trading data(pd.DataFrame): trading data of the target country
+    '''
+    trading_data = {}
+    for k in key_d3:
+        key = f'CompactData/DOT/A.{k}.TMG_FOB_USD.{target_country}\
+            ?startPeriod=2019&endPeriod=2020'
+        data = requests.get(f'{url}{key}').json()['CompactData']['DataSet']['Series']
+        for s in data:
+            df_dict_col = {}
+            if type(s) != dict:
+                continue
+            if 'Obs' not in s.keys() or type(s['Obs']) != list:
+                continue
+            for i in s['Obs']:
+                df_dict_col[i['@TIME_PERIOD']] = round(float(i['@OBS_VALUE']), 1)
+            trading_data[s['@REF_AREA']] = df_dict_col
+    return pd.DataFrame(trading_data).T
