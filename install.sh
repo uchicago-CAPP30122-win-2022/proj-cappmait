d03d7efa1c74a823b79bf2d1e4a1733413c7b278
taishi <taishi1991911@gmail.com>
2022-03-14 19:54:25 -0500

    deleted 2nd, 5th, 6th steps

--- install.sh
+++ install.sh
@@ -19,16 +19,8 @@ fi
 
 echo -e "\t--Your python version is above 3.8, which is enough for this application."
 
-# 2. What OS are we running on?
-PLATFORM=$($PYTHON -c 'import platform; print(platform.system())')
-
-echo -e $PLATFORM
-
-echo -e "2. Checking OS Platform..."
-echo -e "\t--OS=Platform=$PLATFORM"
-
-# 3. Create Virtual environment
-echo -e "3. Creating new virtual environment..."
+# 2. Create Virtual environment
+echo -e "2. Creating new virtual environment..."
 
 # Remove the env directory if it exists 
 if [[ -d env ]]; then
@@ -43,8 +35,8 @@ if [[ ! -d env ]]; then
     exit 1
 fi
 
-# 4. Install Requirements
-echo -e "4. Installing Requirements..."
+# 3. Install Requirements
+echo -e "3. Installing Requirements..."
 if [[ ! -e "requirements.txt" ]]; then 
     echo -e "\t--Need to requirements.txt to install packages."
     exit 1
@@ -54,27 +46,3 @@ source env/bin/activate
 pip3 install -r requirements.txt
 
 echo -e "Install is complete."
-
-# 5. Run the application
-echo -e "5. Running the application..."
-python3 app.py
-
-# 6. Open the demopage
-echo -e "6. Opening the demopage"
-
-URL1=http://127.0.0.1:3003/
-URL2=http://127.0.0.1:3004/
-
-if [[ $PLATFORM == 'Linux' ]];  then
-    xdg-open $URL1
-    xdg-open $URL2
-elif [[ $PLATFORM == 'Darwin' ]];  then
-    open $URL1
-    open $URL2
-elif [[ $PLATFORM == 'Windows' ]];  then
-    start $URL1
-    start $URL2
-else
-    echo -e "Your OS is not supported."
-    exit 1
-fi
